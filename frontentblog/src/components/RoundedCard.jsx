bc7e4a9674aa43230bddf947b0764683623dda86
kyruodioso <kanu_nirvana@hotmail.com>
2024-07-16 17:27:46 -0300

    added workflow style

--- frontentblog/src/components/RoundedCard.jsx
+++ frontentblog/src/components/RoundedCard.jsx
@@ -1,61 +1,92 @@
+import Lottie from "lottie-react"
+import chart from "../assets/lotties/chart.json"
+import cube from "../assets/lotties/cube.json"
+import party from "../assets/lotties/party.json"
+import hammer from "../assets/lotties/hammer.json"
+import qa from "../assets/lotties/qa.json"
+import implementation from "../assets/lotties/implementation.json"
+import design from "../assets/lotties/design.json"
+import feedback from "../assets/lotties/feedback.json"
+
+
+
+
 const workFlow = [
   {
     title: "Planificación",
     description:
       "Se definen objetivos y recursos. Se establece un plan detallado para cada fase del proyecto.",
+      effect: cube
   },
   {
     title: "Análisis de requisitos",
     description:
       "Se determinan las necesidades del sistema. Este punto comprende los requisitos funcionales y no funcionales.",
+      effect: chart
   },
   {
     title: "Diseño",
     description:
       "Se establece la arquitectura de la solución y se diseña la interfaz de usuario (UX/UI).",
+      effect: design
   },
   {
     title: "Implementación",
     description:
       "Se desarrolla el código. Desarrolla funcionalidades según el diseño.",
+      effect: implementation
   },
   {
     title: "Pruebas",
-    description: "Se verifica la funcionalidad. Se busca posibles defectos y errores.",
+    description:
+      "Se verifica la funcionalidad. Se busca posibles defectos y errores.",
+      effect: qa
   },
   {
-    title: "Mantenimiento",
-    description: "Aseguramiento de la operatividad y relevancia a lo largo del tiempo.",
+    title: "Feedback",
+    description:
+      "Se mantiene contacto mediante las pruebas para hacer un seguimiento efectivo.",
+      effect: feedback
   },
   {
     title: "Despliegue a Producción",
     description:
       "Se configura un entorno de producción. Realiza pruebas finales en el entorno de producción. Implementa la aplicación o sitio web en producción.",
+      effect: party
   },
-];
+  {
+    title: "Mantenimiento",
+    description:
+      "Aseguramiento de la operatividad y relevancia a lo largo del tiempo.",
+      effect: hammer
+  },
 
+];
 const RoundedCard = () => {
   return (
     <>
-    {
-      workFlow.map((item)=>(
-       
-            <div key={item.title} className="group relative">
-              <div className="flex flex-col flex-grow bg-white overflow-hidden border border-indigo-500 backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-full">
-                <div className="flex flex-col items-center p-10">
-                  <span className="font-semibold text-white">{item.title}</span>
-          
-                </div>
-                <div className="p-5 text-center text-white">
-                  <p>{item.description}</p>
-                </div>
+      {workFlow.map((item) => (
+        <div key={item.title} className="flex items-center">
+          <div className="group relative">
+            {/* Contenedor del SVG (Lottie) */}
+            <div className="absolute inset-0">
+              <Lottie className="w-full h-full" loop={true} animationData={item.effect} />
+            </div>
+
+            {/* Contenedor del texto */}
+            <div className="h-96 flex flex-col flex-grow bg-white overflow-hidden border border-indigo-500 backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-lg relative">
+              <div className="flex flex-col items-center p-10">
+                <span className="font-semibold text-white">{item.title}</span>
+              </div>
+              <div className="p-5 text-center font-semibold text-white">
+                <p>{item.description}</p>
               </div>
             </div>
-         
-      ))
-    }
+          </div>
+        </div>
+      ))}
     </>
-  )
+  );
 };
 
 export default RoundedCard;
--- frontentblog/src/pages/Home.jsx
+++ frontentblog/src/pages/Home.jsx
@@ -20,7 +20,6 @@ const Home = () => {
         {projects && Array.isArray(projects) && (
           <div>
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-
               <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
                 {projects?.map((project) => (
                   <a
@@ -35,9 +34,7 @@ const Home = () => {
                         className="h-48 w-full object-cover object-center group-hover:opacity-75"
                       />
                     </div>
-                    <h3 className="mt-4 text-sm text-white">
-                      {project.title}
-                    </h3>
+                    <h3 className="mt-4 text-sm text-white">{project.title}</h3>
                     <p className="mt-1 text-lg font-medium text-gray-900">
                       {project.date}
                     </p>
@@ -58,48 +55,47 @@ const Home = () => {
         </div>
         <div className="col mx-4 mt-2 pt-8">
           <p>
-            Iniciando en las mismas ideas con un gratificante diseño visual, una sólida estructura de software y un camino de crecimiento por el proyecto poniendo foco en lo que le dió origen al mismo.
+            Iniciando en las mismas ideas con un gratificante diseño visual, una
+            sólida estructura de software y un camino de crecimiento por el
+            proyecto poniendo foco en lo que le dió origen al mismo.
           </p>
         </div>
       </div>
       <div>
-      <div  className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div
-        
-         className=" grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-<RoundedCard/>
-
-</div>
-</div>
-       
+        <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+          <div className=" grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            <RoundedCard />
+          </div>
+        </div>
       </div>
-
-      <div className="md:grid grid-cols-2 mx-auto max-w-2xl px-4 py-16 sm:px-6  lg:max-w-7xl lg:px-8 text-white ">
-      <div className="col mx-4 mt-2 pr-20">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+        className="absolute z-10 w-full mt-12 hidden md:block"
+      >
+        <path
+          fill="#00cba9"
+          fillOpacity="1"
+          d="M0,96L24,128C48,160,96,224,144,213.3C192,203,240,117,288,85.3C336,53,384,75,432,85.3C480,96,528,96,576,85.3C624,75,672,53,720,80C768,107,816,181,864,208C912,235,960,213,1008,181.3C1056,149,1104,107,1152,106.7C1200,107,1248,149,1296,186.7C1344,224,1392,256,1416,272L1440,288L1440,0L1416,0C1392,0,1344,0,1296,0C1248,0,1200,0,1152,0C1104,0,1056,0,1008,0C960,0,912,0,864,0C816,0,768,0,720,0C672,0,624,0,576,0C528,0,480,0,432,0C384,0,336,0,288,0C240,0,192,0,144,0C96,0,48,0,24,0L0,0Z"
+        ></path>
+      </svg>
+      <div className="md:grid grid-cols-2 mx-auto max-w-2xl px-4 py-16 sm:px-6  lg:max-w-7xl lg:px-8 text-white items-center">
+        <div className="col mx-4 pr-20 z-20">
           <img
             src="https://cdn.pixabay.com/photo/2024/03/11/12/33/crow-8626517_1280.jpg"
             alt=""
           />
         </div>
-        <div className="col mx-4 mt-2 pt-8 bg-white  border-slate-800 backdrop-filter backdrop-blur-sm bg-opacity-10 border p-5 border-b-indigo-500/50 border-l-indigo-500/50 rounded-lg shadow-lg">
-          <span className="text-3-l mb-4 font-semibold">Story of nataliy</span>
-          <h3 className="font-bold text-4xl mb-4">
-            Designing experiences, building connections
-          </h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt minus
-            fugit ipsa. Blanditiis impedit repellendus nobis enim laboriosam
-            doloremque natus, quos placeat molestiae officiis obcaecati, sunt,
-            quo excepturi id tenetur. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Cupiditate nobis error ipsam incidunt dolor, culpa
-            veritatis quod! Nemo laborum ut accusamus veritatis natus expedita
-            et, nobis, placeat, aspernatur voluptate eligendi.
-          </p>
-          <button className="rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-full p-3 mt-8">
+        <div className="col mx-4 z-20">
+          <span className="text-3-l mb-4 font-semibold">Tecnologias</span>
+          <h3 className="font-bold text-4xl mb-4">Diseñando experiencia,</h3>
+          <p>Implementación de las últimas tecnologias</p>
+          <button className="rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-3 mt-8">
             Read more
           </button>
         </div>
       </div>
+
       <div className="w-full text-white mt-8 text-center">
         <span className="text-3-l mb-4 font-semibold">Pricing plan</span>
         <h3 className="font-bold text-4xl text-center">
